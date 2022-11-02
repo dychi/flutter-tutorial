e035c5d85912602fa8efa27d84814c8b1c8694ed
daichi_yamaoka <tnychor06@gmail.com>
2022-11-02 12:00:54 +0900

    画像読み込みアプリ

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import "dart:io";
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,15 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,12 +32,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  String _type = "偶数";
+  XFile? _image;
+  final imagePicker = ImagePicker();
 
-  void _incrementCounter() {
+  // カメラから画像を取得するメソッド
+  Future getImageFromCamera() async {
+    final pickedFile = await imagePicker.pickImage(source: ImageSource.camera);
     setState(() {
-      _counter++;
+      if (pickedFile != null) {
+        _image = XFile(pickedFile.path);
+      }
+    });
+  }
+
+  // ギャラリーから画像を取得するメソッド
+  Future getImageFromGallery() async {
+    final pickedFile = await imagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        _image = XFile(pickedFile.path);
+      }
     });
   }
 
@@ -65,26 +63,24 @@ class _MyHomePageState extends State<MyHomePage> {
           children: const [Icon(Icons.create), Text("タイトル")],
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text("Hello World"),
-          Text(
-            '$_counter',
-            style: Theme.of(context).textTheme.headline4,
+      body: Center(
+          child: _image == null
+              ? const Text("写真を選択してください")
+              : Image.file(File(_image!.path))),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          // カメラから取得するボタン
+          FloatingActionButton(
+            onPressed: getImageFromCamera,
+            child: const Icon(Icons.photo_camera),
           ),
-          if (_counter % 2 == 0) Text("偶数です")
+          // ギャラリーから取得するボタン
+          FloatingActionButton(
+            onPressed: getImageFromGallery,
+            child: const Icon(Icons.photo_album),
+          )
         ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.timer),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-      drawer: const Drawer(
-        child: Center(
-          child: Text("Drawer"),
-        ),
       ),
     );
   }
